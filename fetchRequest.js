6a8e0150b42b7b68dda8fcd9166654b95a7e3683
Hakym <folarinwells@Hakymzs-MacBook-Pro.local>
2022-04-27 14:08:27 +0100

    Added the new functions

---
+++ fetchRequest.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import API from './api';
+import auth from './auth';
+
+export const fetchRequest = async ({
+  authorization = null,
+  path,
+  data = undefined,
+  method = 'POST',
+}) => {
+  if (!authorization) {
+    console.error({status: false, msg: 'Please enter authorization token!'});
+    throw {status: false, msg: 'Please enter authorization token!'};
+  } else {
+    try {
+      const baseURL = API;
+      const instance = axios.create({
+        baseURL,
+        responseType: 'json',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${authorization}`,
+        },
+      });
+
+      try {
+        let response;
+        if (method == 'POST') {
+          response = await instance.post(path, data);
+        } else if (method == 'PUT') {
+          response = await instance.put(path, data);
+        } else {
+          response = await instance.get(path);
+        }
+
+        return response?.data;
+      } catch (error) {
+        // console.error(error?.message || error);
+        throw error;
+      } finally {
+      }
+    } catch (error) {}
+  }
+};
---
+++ indexV2.js
@@ -0,0 +1,100 @@
+import {fetchRequest} from './fetchRequest';
+
+export const getStatementTransactions = async (authorization, statementKey) => {
+  if (!statementKey) {
+    throw {status: false, msg: 'Please include a statement key'};
+  } else {
+    try {
+      const response = await fetchRequest({
+        authorization,
+        path: `/statements/${statementKey}/transactions`,
+        method: 'GET',
+      });
+
+      return {status: true, data: response};
+    } catch (error) {
+      throw {status: false, msg: error?.msg || error};
+    }
+  }
+};
+
+export const getExistingStatementAnalytics = async (
+  authorization,
+  statementKey,
+) => {
+  if (!statementKey) {
+    throw {status: false, msg: 'Please include a statement key'};
+  } else {
+    try {
+      const response = await fetchRequest({
+        authorization,
+        path: `/statements/${statementKey}`,
+        method: 'GET',
+      });
+
+      return {status: true, data: response};
+    } catch (error) {
+      throw {status: false, msg: error?.msg || error};
+    }
+  }
+};
+
+export const generateCreditScoreForAStatement = async (
+  authorization,
+  statementKey,
+) => {
+  if (!statementKey) {
+    throw {status: false, msg: 'Please include a statement key'};
+  } else {
+    try {
+      const response = await fetchRequest({
+        authorization,
+        path: `/creditscore/${statementKey}`,
+        method: 'POST',
+      });
+
+      return {status: true, data: response};
+    } catch (error) {
+      throw {status: false, msg: error?.msg || error};
+    }
+  }
+};
+
+export const getAnExistingCreditScore = async (authorization, statementKey) => {
+  if (!statementKey) {
+    throw {status: false, msg: 'Please include a statement key'};
+  } else {
+    try {
+      const response = await fetchRequest({
+        authorization,
+        path: `/creditscore/${statementKey}`,
+        method: 'GET',
+      });
+
+      return {status: true, data: response};
+    } catch (error) {
+      throw {status: false, msg: error?.msg || error};
+    }
+  }
+};
+
+export const getExistingStatementAffordabilityAnalysis = async (
+  authorization,
+  statementKey,
+) => {
+  if (!statementKey) {
+    throw {status: false, msg: 'Please include a statement key'};
+  } else {
+    try {
+      const response = await fetchRequest({
+        authorization,
+        path: `/affordability/${statementKey}`,
+        method: 'GET',
+      });
+
+      return {status: true, data: response};
+    } catch (error) {
+      throw {status: false, msg: error?.msg || error};
+    }
+  }
+};
